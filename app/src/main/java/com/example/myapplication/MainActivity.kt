ea9c7e4eb56d11d0ec6f3bd2af98f390171937b8
dnfwj <flutter_app>
2020-06-05 13:15:27 +0800

    测试

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        print("2342144134")
         setContentView(R.layout.activity_main)
     }
 }
